d9de3f45e00e333014b73616012bbe848f78a9c8
wb-lq277737 <wb-lq277737@alibaba-inc.com>
2017-07-17 13:53:21 +0800

    debug info : lb

--- footmark/slb/connection.py
+++ footmark/slb/connection.py
@@ -1017,9 +1017,9 @@ class SLBConnection(ACSQueryConnection):
             self.build_list_params(params, load_balancer_id, 'LoadBalancerId')
         if Load_balancer_name:
             self.build_list_params(params, Load_balancer_name, 'LoadBalancerName')
-
-        #return self.get_list('DescribeLoadBalancers', params,  ['Load_balancers', LoadBalancer])
-        return self.get_list('DescribeLoadBalancers', params,  ['LoadBalancers', LoadBalancer])
+        res = self.get_list('DescribeLoadBalancers', params,  ['LoadBalancers', LoadBalancer])
+        print dir(res)
+        return res
         
     def create_vserver_group(self, load_balancer_id, vserver_group_name, backend_servers):
         """
